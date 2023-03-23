9b7eb57fdb2e527711be955e433df8b9041b49d7
vk196 <victorumeh196@gmail.com>
2023-03-23 08:08:22 -0700

    completed project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
+
 const redLight = document.querySelector(".red");
 const greenLight = document.querySelector(".green");
+const bread = document.querySelector(".bread");
 const breadSmoke = document.getElementById("smoke");
 const toastHandle = document.querySelector(".handle__wrapper");
 
@@ -9,9 +11,12 @@ console.log(breadSmoke);
 greenLight.addEventListener("click", () => {
   toastHandle.classList.add("active");
   breadSmoke.classList.add("active");
+  bread.classList.add("active");
 });
 
 //RedLight func
 redLight.addEventListener("click", () => {
   toastHandle.classList.remove("active");
+  breadSmoke.classList.remove("active");
+  bread.classList.remove("active");
 });
