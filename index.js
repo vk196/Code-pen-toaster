e76bfeb15487f2de79dbacd39d0a38999f22f0ef
vk196 <victorumeh196@gmail.com>
2023-03-23 04:24:22 -0700

    Implemented handle function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 "use strict";
 const redLight = document.querySelector(".red");
 const greenLight = document.querySelector(".green");
+const breadSmoke = document.getElementById("smoke");
+const toastHandle = document.querySelector(".handle__wrapper");
+
+console.log(breadSmoke);
+//GreenLight func
+greenLight.addEventListener("click", () => {
+  toastHandle.classList.add("active");
+  breadSmoke.classList.add("active");
+});
+
+//RedLight func
+redLight.addEventListener("click", () => {
+  toastHandle.classList.remove("active");
+});
